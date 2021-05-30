55c536029c61bd2a7231e19fe80c3698057acad8
Shih-Min Lee <ssmlee04@Shih-Mins-MacBook-Pro.local>
2021-05-30 18:05:21 -0400

    babel transform css

--- dist/index.js
+++ dist/index.js
@@ -15,8 +15,6 @@ var _reactChartjs = require("react-chartjs-2");
 
 var _reactCopyToClipboard = require("react-copy-to-clipboard");
 
-require("./../index.css");
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
